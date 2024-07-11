2c2debaa7b9eda930dda53d975bd7cd6d15d359e
oscarcordero <oscar.cordero201318@gmail.com>
2024-07-11 10:31:49 -0500

    Create a custom textbox javascript to add a custom text box to the html

---
+++ js/customTextbox.js
@@ -0,0 +1,69 @@
+
+/* Create HTML inner text for textbox */
+let textboxInnerText = `
+<input type="text" id="textbox-input" placeholder="Enter Text Here...">
+<span></span>
+`;
+
+
+/* Create Style for textbox */
+let textboxStyle = `
+#textbox-input {
+    background: transparent;
+    border: none;
+    width: 100%;
+    height: 100%;
+    padding-left: 15px;
+    outline: none;
+    font-size: 1em;
+}
+
+.textbox-div {
+    --clr: rgba(112, 14, 204, 0.5);
+    --w: 400px;
+    --h: 30px;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid grey;
+    border-radius: 5px;
+    overflow: hidden;
+    width: var(--w);
+    height: var(--h);
+}
+
+#textbox-input:focus ~ span {
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background: var(--clr);
+    bottom: 0px;
+}
+`;
+
+/* Convert any div.textbox-div to the custom textbox */
+let newTextbox = document.querySelectorAll('.textbox-div');
+newTextbox.forEach(element => {
+    // If there is an Arial Placeholder then it will be passed as the textbox placeholder
+    if (element.ariaPlaceholder) {
+        element.innerHTML = `
+            <input type="text" id="textbox-input" placeholder="${element.ariaPlaceholder}">
+            <span></span>
+        `;
+    } else {
+        element.innerHTML = textboxInnerText;
+    }
+});
+
+/* Add style to the textbox */
+// If there is no style element then it will create one
+if (document.querySelector('head style')) {
+    let styleElement = document.querySelector('head style');
+    styleElement.textContent += textboxStyle;
+} else {
+    let newStyleElement = document.createElement('style');
+    newStyleElement.textContent = textboxStyle;
+    document.querySelector('head').appendChild(newStyleElement);
+}
+
