5765881d74a5fed09d11d532b0feb84de268fa42
oscarcordero <oscar.cordero201318@gmail.com>
2024-07-25 09:48:36 -0500

    React Test Folder added

---
+++ ReactTest/my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <div class="search-bar">
+    <input type="text" placeholder="Search"/>
+    <span className="material-symbols-outlined">search</span>
+  </div>
+  );
+}
+
+export default App;
